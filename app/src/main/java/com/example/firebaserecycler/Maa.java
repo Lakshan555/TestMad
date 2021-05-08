83fba263147b893f8c5385b5727fa8ad244faf9e
IshankaPramidu <onlineipl18@gmail.com>
2021-05-08 17:47:28 +0530

    ado aye

---
+++ app/src/main/java/com/example/firebaserecycler/Maa.java
@@ -0,0 +1,14 @@
+package com.example.firebaserecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Maa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maa);
+    }
+}
