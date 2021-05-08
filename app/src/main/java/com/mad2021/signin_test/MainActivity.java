8397e9ecca361c0ddaae2dd44993f371fc6ada18
Nuradha <nuradhachamikara@gmail.com>
2021-05-08 17:34:28 +0530

    test sign

---
+++ app/src/main/java/com/mad2021/signin_test/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mad2021.signin_test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Initialize
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, signup.class));
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
